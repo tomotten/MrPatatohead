bb523f8386770fb14284828fdcc7524fa5a75592
tomotten <tom_otten@live.nl>
2018-11-01 14:32:52 +0100

    First working version

--- app/src/main/java/com/mrpatatohead/tom/mrpatatohead/MainActivity.java
+++ app/src/main/java/com/mrpatatohead/tom/mrpatatohead/MainActivity.java
@@ -1,7 +1,12 @@
 package com.mrpatatohead.tom.mrpatatohead;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkClicked(View v) {
+//        Log.d("potato", "checkClicked: ");
+        CheckBox checkbox = (CheckBox) v;
+        String name = "image" + checkbox.getText().toString();
+        Context context = this;
+        int imgId = getResources().getIdentifier(name, "id", context.getPackageName());
+        ImageView image = (ImageView) findViewById(imgId);
+        if (image.getVisibility() == View.VISIBLE) {
+            image.setVisibility(View.INVISIBLE);
+        }
+        else {
+            image.setVisibility(View.VISIBLE);
+        }
+    }
 }
+
+
